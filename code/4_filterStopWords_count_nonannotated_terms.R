1d1b3a2e45dba9f0809c1140fa8b87066e9364b1
Samantha Csik <samanthacsik@gmail.com>
2020-10-13 13:38:30 -0700

    renamed to ensure it's clear that this is for nonannotated terms only

--- code/4_filterStopWords_count_nonannotated_terms.R
+++ code/4_filterStopWords_count_nonannotated_terms.R
@@ -1,7 +1,7 @@
-# title: Unnest NONANNOTATED attributes into individual words, bigrams, trigrams
+# title: Remove stop words and calculate term frequncies for NONANNOTATED attributes
 # author: "Sam Csik"
 # date created: "2020-10-02"
-# date edited: "2020-10-02"
+# date edited: "2020-10-12"
 # R version: 3.6.3
 # input: 
 # output: 
@@ -10,31 +10,27 @@
 # Summary
 ##########################################################################################
 
-# nonannotated attributes
+# This script uses the tidytext package to filter out "stop_words" from unnested token files, count number of occurrences for each term (token), and count the number of unique identifiers & unique authors associated with each unique term
+# These data frames are saved then as .csv files 
 
 ##############################
-# Load packages
+# Load packages & custom functions
 ##############################
 
 source(here::here("code", "0_libraries.R"))
-
-##############################
-# Load custom functions
-##############################
-
 source(here::here("code", "0_functions.R"))
 
 ##########################################################################################
 # Filter/count INDIVIDUAL TERMS
 ##########################################################################################
 
-# isolate individual token files #####(do not include attributes -- they are in a different format)
-nonannotated_unnested_indiv_files <- list.files(path = here::here("data", "unnested_tokens", "nonannotated_attributes"), pattern = glob2rx("unnested_*Indiv*"))
+# isolate individual token files 
+nonannotated_unnested_indiv_files <- list.files(path = here::here("data", "unnested_terms", "nonannotated_attributes2020-10-12"), pattern = glob2rx("unnested_*Indiv*"))
 
 # remove excess columns, filter out stop_words, remove NAs, calculate counts
 for(i in 1:length(nonannotated_unnested_indiv_files)){
   file_name <- nonannotated_unnested_indiv_files[i]
-  file_path <- "data/unnested_tokens/nonannotated_attributes"
+  file_path <- "data/unnested_terms/nonannotated_attributes2020-10-12"
   print(file_name)
   filterCount_indivTerms(file_path, file_name)
 }
@@ -44,12 +40,12 @@ for(i in 1:length(nonannotated_unnested_indiv_files)){
 ##########################################################################################
 
 # isolate individual token files #####(do not include attributes -- they are in a different format)
-nonannotated_unnested_bigram_files <- list.files(path = here::here("data", "unnested_tokens", "nonannotated_attributes"), pattern = glob2rx("unnested_*Bigram*"))
+nonannotated_unnested_bigram_files <- list.files(path = here::here("data", "unnested_terms", "nonannotated_attributes2020-10-12"), pattern = glob2rx("unnested_*Bigram*"))
 
 # remove excess columns, filter out stop_words, remove NAs, calculate counts
 for(i in 1:length(nonannotated_unnested_bigram_files)){
   file_name <- nonannotated_unnested_bigram_files[i]
-  file_path <- "data/unnested_tokens/nonannotated_attributes"
+  file_path <- "data/unnested_terms/nonannotated_attributes2020-10-12"
   print(file_name)
   filterCount_bigramTerms(file_path, file_name)
 }
@@ -59,12 +55,12 @@ for(i in 1:length(nonannotated_unnested_bigram_files)){
 ##########################################################################################
 
 # isolate individual token files #####(do not include attributes -- they are in a different format)
-nonannotated_unnested_trigram_files <- list.files(path = here::here("data", "unnested_tokens", "nonannotated_attributes"), pattern = glob2rx("unnested_*Trigram*"))
+nonannotated_unnested_trigram_files <- list.files(path = here::here("data", "unnested_terms", "nonannotated_attributes2020-10-12"), pattern = glob2rx("unnested_*Trigram*"))
 
 # remove excess columns, filter out stop_words, remove NAs, calculate counts
 for(i in 1:length(nonannotated_unnested_trigram_files)){
   file_name <- nonannotated_unnested_trigram_files[i]
-  file_path <- "data/unnested_tokens/nonannotated_attributes"
+  file_path <- "data/unnested_terms/nonannotated_attributes2020-10-12"
   print(file_name)
   filterCount_trigramTerms(file_path, file_name)
 }
@@ -76,11 +72,22 @@ for(i in 1:length(nonannotated_unnested_trigram_files)){
 # get list of new dfs
 df_list <- mget(ls(pattern = "filteredCounts_"))
 
-# function to write as .csv files to appropriate subdirectory
-output_csv <- function(data, names){
-  write_csv(data, here::here("data", "filtered_term_counts", "nonannotated_attributes", paste0(names, ".csv")))
+# save .csv files
+for (i in 1:length(df_list)){
+  data <- df_list[[i]]
+  names <- names(df_list)[i]
+  file_path <- "data/filtered_term_counts/nonannotated_attributes2020-10-12"
+  output_csv(data, names, file_path)
 }
 
-# write each df as .csv file
-list(data = df_list, names = names(df_list)) %>%
-  purrr::pmap(output_csv) 
+# # get list of new dfs
+# df_list <- mget(ls(pattern = "filteredCounts_"))
+# 
+# # function to write as .csv files to appropriate subdirectory
+# output_csv <- function(data, names){
+#   write_csv(data, here::here("data", "filtered_term_counts", "nonannotated_attributes", paste0(names, ".csv")))
+# }
+# 
+# # write each df as .csv file
+# list(data = df_list, names = names(df_list)) %>%
+#   purrr::pmap(output_csv) 
