afb8c0abc703c1f2a87cf5d36d539da3e77513af
Samantha Csik <samanthacsik@gmail.com>
2021-02-01 15:41:22 -0800

    reorganized and annoated for clarity

--- code/functions/annotate_attributes().R
+++ code/functions/annotate_attributes().R
@@ -1,46 +1,87 @@
 ##############################
-# annotate attributes (NOTE THERE ARE TWO DIFFERENT FUNCTIONS HERE)
+# annotate attributes from PACKED entities (i.e. when there are more than one entity present)
+  # doc: eml metadata 
+  # entity_num: index of entity (see for loop in 'process_entites_by_type()')
+  # dataTable_or_otherEntity: character string, "dataTable" or "otherEntity"
+  # current_eml_entity: the entity from doc at index 'entity_num' (i.e. all_named_entities[[entity_num]])
 ##############################
 
-annotate_attributes <- function(doc, entity_num, current_entityName_from_eml, num_attributes_in_eml_entity, current_entity_subset, entity_path){
+annotate_attributes <- function(doc, entity_num, dataTable_or_otherEntity, current_eml_entity){ 
   
+  # message("working on: ", entity_num, " for: ", dataTable_or_otherEntity)
+  
+  # initiate annotation counter to track number of annotations added to doc
   annotation_counter <- 0
+  
+  # get current dataTable or otherEntity name from eml metadata; determine how many attributes exist in the eml entity
+  current_entityName_from_eml <- current_eml_entity$entityName
+  num_attributes_in_eml_entity <- length(current_eml_entity$attributeList$attribute)
+  
+  # subset 'current_datapackage_subset' accordingly
+  current_entity_subset <- current_datapackage_subset %>%
+    filter(entityName == current_entityName_from_eml)
   
   for(att_num in 1:num_attributes_in_eml_entity){
     
-    # 3.1) get attribute from dataTable in eml 
-    current_attribute_name_from_eml <- entity_path[[entity_num]]$attributeList$attribute[[att_num]]$attributeName
+    # get attributeName fom entity in eml 
+    current_attribute_name_from_eml <- current_eml_entity$attributeList$attribute[[att_num]]$attributeName
     # message("--> Found attribute #", att_num, " : '", current_attribute_name_from_eml, "'")
     
-    # 3.2) subset df using current_attribute_name_from_eml 
+    # subset df using current_attribute_name_from_eml 
     current_attribute_subset <- current_entity_subset %>% 
       filter(attributeName == current_attribute_name_from_eml)
     
-    # 3.3) if eml attribute exists in df, continue, if not move to next attribute in eml
+    # if eml attribute exists in df, continue, if not move to next attribute in eml
     if(length(current_attribute_subset$attributeName > 0)){ 
-      
       # message("The corresponding attribute to #", att_num, " in the df is: '", current_attribute_subset$attributeName, "'")
       
-      # 3.4) create attribute id 
-      current_attribute_id <- build_attributeID(entity_num, att_num, entity_path)
+      # create attribute id using entity index (entity_num) and attributeName from eml
+      current_attribute_id <- build_attributeID(entity_num, att_num, eml_get(doc$dataset, dataTable_or_otherEntity))
       
-      # 3.5) verify that the attribute id is unique across datapackage
+      # verify that the attribute id is unique across datapackage
       verify_attributeID_isUnique(current_attribute_id = current_attribute_id)
       
-      # 3.6) add attribute id to metadata
-      entity_path[[entity_num]]$attributeList$attribute[[att_num]]$id <- current_attribute_id
-      # message("Added attributeID, '", current_attribute_id, "' to metadata")
+      #---------------------------------------------------------------------------------------------
+      # add semantic annotation elements to dataTables
+      #---------------------------------------------------------------------------------------------
       
-      # 3.7) create/add property URI to metadata (same for all attributes)
-      entity_path[[entity_num]]$attributeList$attribute[[att_num]]$annotation$propertyURI <- list(label = "contains meausurements of",
-                                                                                                        propertyURI = "http://ecoinformatics.org/oboe/oboe.1.2/oboe-core.owl#containsMeasurementsOfType")
-      
-      # 3.8) add value URI to metadata
-      current_valueURI <- current_attribute_subset$assigned_valueURI
-      current_label <- current_attribute_subset$prefName
-      entity_path[[entity_num]]$attributeList$attribute[[att_num]]$annotation$valueURI <- list(label = current_label,
-                                                                                                     valueURI = current_valueURI)
-      # message("Added semantic annotation URI, '", current_valueURI, "' to metadata for attribute, '", current_attribute_name_from_eml, "'")
+      if (dataTable_or_otherEntity == 'dataTable') {
+        
+        # add attribute id to metadata 
+        doc$dataset$dataTable[[entity_num]]$attributeList$attribute[[att_num]]$id <- current_attribute_id
+        # message("Added attributeID, '", current_attribute_id, "' to metadata")
+        
+        # add property URI to metadata (this is the same for all attributes) 
+        containsMeasurementsofType <- "http://ecoinformatics.org/oboe/oboe.1.2/oboe-core.owl#containsMeasurementsOfType"
+        doc$dataset$dataTable[[entity_num]]$attributeList$attribute[[att_num]]$annotation$propertyURI <- list(label = "contains meausurements of",
+                                                                                                              propertyURI = containsMeasurementsofType)
+        
+        # add value URI to metadata 
+        current_valueURI <- current_attribute_subset$assigned_valueURI
+        current_label <- current_attribute_subset$prefName
+        doc$dataset$dataTable[[entity_num]]$attributeList$attribute[[att_num]]$annotation$valueURI <- list(label = current_label,
+                                                                                                           valueURI = current_valueURI)
+        # message("Added semantic annotation URI, '", current_valueURI, "' to metadata for attribute, '", current_attribute_name_from_eml, "'")
+        
+        #---------------------------------------------------------------------------------------------
+        # add semantic annotation elements to otherEntities
+        #---------------------------------------------------------------------------------------------
+
+      } else {
+        doc$dataset$otherEntity[[entity_num]]$attributeList$attribute[[att_num]]$id <- current_attribute_id
+        # message("Added attributeID, '", current_attribute_id, "' to metadata")
+        
+        # 3.7) create/add property URI to metadata (same for all attributes)
+        containsMeasurementsofType <- "http://ecoinformatics.org/oboe/oboe.1.2/oboe-core.owl#containsMeasurementsOfType"
+        doc$dataset$otherEntity[[entity_num]]$attributeList$attribute[[att_num]]$annotation$propertyURI <- list(label = "contains meausurements of",
+                                                                                                                propertyURI = containsMeasurementsofType)
+        # 3.8) add value URI to metadata !!!!!!!!!!!!!!!!!
+        current_valueURI <- current_attribute_subset$assigned_valueURI
+        current_label <- current_attribute_subset$prefName
+        doc$dataset$otherEntity[[entity_num]]$attributeList$attribute[[att_num]]$annotation$valueURI <- list(label = current_label,
+                                                                                                             valueURI = current_valueURI)
+        # message("Added semantic annotation URI, '", current_valueURI, "' to metadata for attribute, '", current_attribute_name_from_eml, "'")
+      }
       
       annotation_counter <- annotation_counter + 1
       
@@ -54,7 +95,6 @@ annotate_attributes <- function(doc, entity_num, current_entityName_from_eml, nu
   }
   
   # messaging for mental checks
-  print("used 'annotate_attributes() function'")
   attributes_to_annotate <- length(current_entity_subset$attributeName)
   message("Processed entity: ", entity_num, " | ",  current_entityName_from_eml)
   message("Attributes in Metadata -> ", num_attributes_in_eml_entity, " | attributes to annotate -> ", attributes_to_annotate, " | Added -> ", annotation_counter, " | Complete -> ", (attributes_to_annotate == annotation_counter))
@@ -64,50 +104,125 @@ annotate_attributes <- function(doc, entity_num, current_entityName_from_eml, nu
 }
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# some room to breathe...
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 ##############################
 # annotated attributes from UNPACKED entities (i.e. when there is only ONE entity present and it's elements are not collapsed inside a nested list)
+  # doc: eml metadata
+  # dataTable_or_otherEntity: character string, "dataTable" or "otherEntity"
+  # current_eml_entity: the entity from doc at index 'entity_num' (i.e. all_named_entities[[entity_num]])
 ##############################
 
-annotate_attributes_unpacked <- function(doc, current_entityName_from_eml, num_attributes_in_eml_entity, current_entity_subset, entity_path){
+annotate_attributes_unpacked <- function(doc, dataTable_or_otherEntity, current_eml_entity){
   
+  # initiate annotation counter to track number of annotations added to doc
   annotation_counter <- 0
+  
+  # get current dataTable or otherEntity name from metadata; determine how many attributes exist in the eml entity
+  current_entityName_from_eml <- current_eml_entity$entityName
+  num_attributes_in_eml_entity <- length(current_eml_entity$attributeList$attribute)
+  
+  # subset 'current_datapackage_subset' accordingly
+  current_entity_subset <- current_datapackage_subset %>%
+    filter(entityName == current_entityName_from_eml)
   
   for(att_num in 1:num_attributes_in_eml_entity){
     
-    # 3.1) get attribute from dataTable in eml 
-    current_attribute_name_from_eml <- entity_path$attributeList$attribute[[att_num]]$attributeName
+    # get attribute from dataTable or otherEntity in eml 
+    current_attribute_name_from_eml <- current_eml_entity$attributeList$attribute[[att_num]]$attributeName
     # message("--> Found attribute #", att_num, " : '", current_attribute_name_from_eml, "'")
     
-    # 3.2) subset df using current_attribute_name_from_eml 
+    # subset df using current_attribute_name_from_eml 
     current_attribute_subset <- current_entity_subset %>% 
       filter(attributeName == current_attribute_name_from_eml)
     
-    # 3.3) if eml attribute exists in df, continue, if not move to next attribute in eml
+    # if eml attribute exists in df, continue, if not move to next attribute in eml
     if(length(current_attribute_subset$attributeName > 0)){ 
       
       # message("The corresponding attribute to #", att_num, " in the df is: '", current_attribute_subset$attributeName, "'")
       
-      # 3.4) create attribute id 
-      current_attribute_id <- build_attributeID(entity_num, att_num, entity_path)
+      # create attribute id
+      current_attribute_id <- build_attributeID(entity_num, att_num, eml_get(doc$dataset, dataTable_or_otherEntity))
       
-      # 3.5) verify that the attribute id is unique across datapackage
+      # verify that the attribute id is unique across datapackage
       verify_attributeID_isUnique(current_attribute_id = current_attribute_id)
       
-      # 3.6) add attribute id to metadata
-      entity_path$attributeList$attribute[[att_num]]$id <- current_attribute_id
-      # message("Added attributeID, '", current_attribute_id, "' to metadata")
+      #---------------------------------------------------------------------------------------------
+      # add semantic annotation elements to dataTables
+      #---------------------------------------------------------------------------------------------
       
-      # 3.7) create/add property URI to metadata (same for all attributes)
-      entity_path$attributeList$attribute[[att_num]]$annotation$propertyURI <- list(label = "contains meausurements of",
-                                                                                    propertyURI = "http://ecoinformatics.org/oboe/oboe.1.2/oboe-core.owl#containsMeasurementsOfType")
-      
-      # 3.8) add value URI to metadata
-      current_valueURI <- current_attribute_subset$assigned_valueURI
-      current_label <- current_attribute_subset$prefName
-      entity_path$attributeList$attribute[[att_num]]$annotation$valueURI <- list(label = current_label,
-                                                                                 valueURI = current_valueURI)
-      
-      # message("Added semantic annotation URI, '", current_valueURI, "' to metadata for attribute, '", current_attribute_name_from_eml, "'")
+      if (dataTable_or_otherEntity == 'dataTable') {
+        
+        # add attribute id to metadata
+        doc$dataset$dataTable[[entity_num]]$attributeList$attribute[[att_num]]$id <- current_attribute_id
+        # message("Added attributeID, '", current_attribute_id, "' to metadata")
+        
+        # create/add property URI to metadata (same for all attributes)
+        containsMeasurementsofType <- "http://ecoinformatics.org/oboe/oboe.1.2/oboe-core.owl#containsMeasurementsOfType"
+        doc$dataset$dataTable[[entity_num]]$attributeList$attribute[[att_num]]$annotation$propertyURI <- list(label = "contains meausurements of",
+                                                                                                              propertyURI = containsMeasurementsofType)
+        
+        # add value URI to metadata
+        current_valueURI <- current_attribute_subset$assigned_valueURI
+        current_label <- current_attribute_subset$prefName
+        doc$dataset$dataTable[[entity_num]]$attributeList$attribute[[att_num]]$annotation$valueURI <- list(label = current_label,
+                                                                                                           valueURI = current_valueURI)
+        # message("Added semantic annotation URI, '", current_valueURI, "' to metadata for attribute, '", current_attribute_name_from_eml, "'")
+        
+        #---------------------------------------------------------------------------------------------
+        # add semantic annotation elements to otherEntities
+        #---------------------------------------------------------------------------------------------
+        
+      } else {
+        
+        # add attribute id to metadata
+        doc$dataset$otherEntity[[entity_num]]$attributeList$attribute[[att_num]]$id <- current_attribute_id
+        # message("Added attributeID, '", current_attribute_id, "' to metadata")
+       
+         # create/add property URI to metadata (same for all attributes)
+        containsMeasurementsofType <- "http://ecoinformatics.org/oboe/oboe.1.2/oboe-core.owl#containsMeasurementsOfType"
+        doc$dataset$otherEntity[[entity_num]]$attributeList$attribute[[att_num]]$annotation$propertyURI <- list(label = "contains meausurements of",
+                                                                                                                propertyURI = containsMeasurementsofType)
+       
+         # add value URI to metadata
+        current_valueURI <- current_attribute_subset$assigned_valueURI
+        current_label <- current_attribute_subset$prefName
+        doc$dataset$otherEntity[[entity_num]]$attributeList$attribute[[att_num]]$annotation$valueURI <- list(label = current_label,
+                                                                                                             valueURI = current_valueURI)
+        # message("Added semantic annotation URI, '", current_valueURI, "' to metadata for attribute, '", current_attribute_name_from_eml, "'")
+      }
       
       annotation_counter <- annotation_counter + 1
       
@@ -120,7 +235,6 @@ annotate_attributes_unpacked <- function(doc, current_entityName_from_eml, num_a
   }
   
   # messaging for mental checks
-  print("used 'annotate_attributes_unpacked() function'")
   attributes_to_annotate <- length(current_entity_subset$attributeName)
   message("Processed entity: 1 (only one) | ",  current_entityName_from_eml)
   message("Attributes in Metadata -> ", num_attributes_in_eml_entity, " | attributes to annotate -> ", attributes_to_annotate, " | Added -> ", annotation_counter, " | Complete -> ", (attributes_to_annotate == annotation_counter))
