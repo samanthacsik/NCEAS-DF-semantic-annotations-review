42e95ac65f93555f6aff45ab4eaeb509ffa7c56e
Samantha Csik <samanthacsik@gmail.com>
2021-02-01 15:39:31 -0800

    updated variable names for consistency with other functions; added info about arguments

--- code/functions/build_attribute_id().R
+++ code/functions/build_attribute_id().R
@@ -1,23 +1,23 @@
 ##############################
-# build attribute id
-  # NOTE CHANGES:
-      # dataTable_number --> entity_number
-      # doc$dataset$dataTable --> entity_path
+# build attribute id using index of current entity and attributeName from eml
+  # entity_num: index of entity
+  # attribute_num: index of attribute in current entity attribute
+  # entity_path: 'eml_get(doc$dataset, dataTable_or_otherEntity)'
 ##############################
 
-build_attributeID <- function(entity_number, attribute_number, entity_path){
+build_attributeID <- function(entity_num, attribute_num, entity_path){
   
   # if multiple entities present
-  if(isTRUE(is.list(all_entities_path[[1]]))){
-    entity_name <- tolower(paste("entity", entity_number, sep = "")) 
-    attribute_name <- tolower(entity_path[[entity_number]]$attributeList$attribute[[attribute_number]]$attributeName)
+  if(isTRUE(is.list(entity_path[[1]]))){
+    entity_name <- tolower(paste("entity", entity_num, sep = "")) 
+    attribute_name <- tolower(entity_path[[entity_num]]$attributeList$attribute[[attribute_num]]$attributeName)
     attribute_name_combo <- (paste("attribute", attribute_name, sep = "_")) 
     attribute_id <- paste(entity_name, attribute_name, sep = "_")
     
     # if single, unpacked entity present
-  } else if(isTRUE(is.character(all_entities_path[[1]]))){
-    entity_name <- tolower(paste("entity", entity_number, sep = "")) 
-    attribute_name <- tolower(entity_path$attributeList$attribute[[attribute_number]]$attributeName)
+  } else if(isTRUE(is.character(entity_path[[1]]))){
+    entity_name <- tolower(paste("entity", entity_num, sep = "")) 
+    attribute_name <- tolower(entity_path$attributeList$attribute[[attribute_num]]$attributeName)
     attribute_name_combo <- (paste("attribute", attribute_name, sep = "_")) 
     attribute_id <- paste(entity_name, attribute_name, sep = "_")
   }
