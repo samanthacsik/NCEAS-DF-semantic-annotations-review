91b1a415d25ff22fa867941a29a4acbda8fc0710
Samantha Csik <samanthacsik@gmail.com>
2021-03-10 10:24:27 -0800

    removed old uncommented code

--- code/batchUpdate_functions/build_attributeID().R
+++ code/batchUpdate_functions/build_attributeID().R
@@ -44,30 +44,3 @@ build_attributeID <- function(entity_num, eml_att_num, entity_path, current_eml_
     }
   }
 }
-  
-
-
-
-
-
-
-
-# build_attributeID <- function(entity_num, attribute_num, entity_path){
-#   
-#   # if multiple entities present
-#   if(isTRUE(is.list(entity_path[[1]]))){
-#     entity_name <- tolower(paste("entity", entity_num, sep = "")) 
-#     attribute_name <- tolower(entity_path[[entity_num]]$attributeList$attribute[[attribute_num]]$attributeName)
-#     attribute_name_combo <- (paste("attribute", attribute_name, sep = "_")) 
-#     attribute_id <- paste(entity_name, attribute_name, sep = "_")
-#     
-#     # if single, unpacked entity present
-#   } else if(isTRUE(is.character(entity_path[[1]]))){
-#     entity_name <- tolower(paste("entity", entity_num, sep = "")) 
-#     attribute_name <- tolower(entity_path$attributeList$attribute[[attribute_num]]$attributeName)
-#     attribute_name_combo <- (paste("attribute", attribute_name, sep = "_")) 
-#     attribute_id <- paste(entity_name, attribute_name, sep = "_")
-#   }
-#   
-#   return(attribute_id)
-# }
